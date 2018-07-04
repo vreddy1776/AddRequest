392b816d96af4c3d93b981b2df6f7deaa261f2c0
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-04 17:24:52 -0500

    Added MainViewModel

---
+++ app/src/main/java/com/example/android/addrequest/MainViewModel.java
@@ -0,0 +1,13 @@
+package com.example.android.addrequest;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.util.Log;
+
+public class MainViewModel extends AndroidViewModel{
+
+    public MainViewModel(Application application) {
+        super(application);
+    }
+
+}

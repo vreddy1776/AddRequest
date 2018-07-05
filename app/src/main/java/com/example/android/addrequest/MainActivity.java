e730ea571bedbfd0231e2a48095a22c66114c999
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-04 20:16:05 -0500

    Added Intent for navigating to RequesterDetails

--- app/src/main/java/com/example/android/addrequest/MainActivity.java
+++ app/src/main/java/com/example/android/addrequest/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.android.addrequest;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViewModel();
+
     }
+
+    private void setupViewModel() {
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+    }
+
+    /** Navigate to RequesterDetails Activity. */
+    public void toRequesterDetails(View view){
+        Intent intent = new Intent(MainActivity.this, RequesterDetailsActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/android/addrequest/MainViewModel.java
+++ app/src/main/java/com/example/android/addrequest/MainViewModel.java
@@ -4,6 +4,11 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.util.Log;
 
+/**
+ * ViewModel for MainActivity.
+ */
+
+
 public class MainViewModel extends AndroidViewModel{
 
     public MainViewModel(Application application) {

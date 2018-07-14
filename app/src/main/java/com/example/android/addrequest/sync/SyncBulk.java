6c807138390080b8af5228fbdf6656abe334cdbf
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-13 22:56:23 -0500

    SyncBulk - updated to keep all ticket syncs on a single thread

--- app/src/main/java/com/example/android/addrequest/sync/SyncBulk.java
+++ app/src/main/java/com/example/android/addrequest/sync/SyncBulk.java
@@ -23,40 +23,38 @@ public class SyncBulk {
     private AppDatabase database;
 
 
-    public void bulkPopulate(Context context, JSONArray jsonArray){
+    public void bulkPopulate(Context context, final JSONArray jsonArray){
 
         database = AppDatabase.getInstance(context);
 
-        for ( int i = 0 ; i < jsonArray.length() ; i++) {
-            try {
+        AppExecuters.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
 
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                for ( int i = 0 ; i < jsonArray.length() ; i++) {
 
-                Log.d(TAG, "JSONobject:  " + jsonObject );
-                Log.d(TAG, "id:  " + jsonObject.get("id") );
-                Log.d(TAG, "title:  " + jsonObject.get("title") );
-                Log.d(TAG, "description:  " + jsonObject.get("description") );
-                Log.d(TAG, "date:  " + jsonObject.get("date") );
+                    try {
 
-                int id = Integer.parseInt(jsonObject.get("id").toString());
-                String title = jsonObject.get("title").toString();
-                String description = jsonObject.get("description").toString();
-                String dateString = jsonObject.get("date").toString();
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        
+                        int id = Integer.parseInt(jsonObject.get("id").toString());
+                        String title = jsonObject.get("title").toString();
+                        String description = jsonObject.get("description").toString();
+                        String dateString = jsonObject.get("date").toString();
 
-                Date date = DateConverter.stringToDate(dateString);
-                final TicketEntry ticket = new TicketEntry(id, title, description, date);
+                        Date date = DateConverter.stringToDate(dateString);
+                        final TicketEntry ticket = new TicketEntry(id, title, description, date);
 
-                AppExecuters.getInstance().diskIO().execute(new Runnable() {
-                    @Override
-                    public void run() {
                         database.ticketDao().insertTicket(ticket);
-                    }
-                });
 
-            } catch (JSONException e) {
-                e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
             }
-        }
+        });
 
     }
 

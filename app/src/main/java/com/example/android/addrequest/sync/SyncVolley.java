01cca63414139d6e5935edeecd099b5ba1561fe5
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-14 11:30:43 -0500

    SyncVolley - URL update

--- app/src/main/java/com/example/android/addrequest/sync/SyncVolley.java
+++ app/src/main/java/com/example/android/addrequest/sync/SyncVolley.java
@@ -20,7 +20,7 @@ public class SyncVolley {
     private static final String TAG = SyncVolley.class.getSimpleName();
 
     private static final String MAIN_URL =
-            "http://ec2-18-191-155-187.us-east-2.compute.amazonaws.com/requests/";
+            "http://ec2-18-219-46-19.us-east-2.compute.amazonaws.com/requests/";
 
     public void getJSON(final Context context){
 

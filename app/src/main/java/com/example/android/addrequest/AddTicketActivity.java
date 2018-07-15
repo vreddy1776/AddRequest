6dc87a3cc1269ed558b569f6ed1a17385d8538b2
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-15 16:28:58 -0500

    Completed test for AWS S3 video loading

--- app/src/main/java/com/example/android/addrequest/AddTicketActivity.java
+++ app/src/main/java/com/example/android/addrequest/AddTicketActivity.java
@@ -4,20 +4,34 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import com.example.android.addrequest.database.AppDatabase;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.mobile.auth.core.IdentityHandler;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import com.amazonaws.services.s3.S3ClientOptions;
 import com.example.android.addrequest.database.TicketEntry;
 import com.example.android.addrequest.utils.GenerateID;
 
+import java.io.File;
 import java.util.Date;
 
 public class AddTicketActivity extends AppCompatActivity{
@@ -47,11 +61,48 @@ public class AddTicketActivity extends AppCompatActivity{
     EditText mDescriptionText;
     Button mButton;
 
+    private AWSCredentialsProvider credentialsProvider;
+    private AWSConfiguration configuration;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ticket);
 
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+
+                // Obtain the reference to the AWSCredentialsProvider and AWSConfiguration objects
+                credentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
+                configuration = AWSMobileClient.getInstance().getConfiguration();
+
+                // Use IdentityManager#getUserID to fetch the identity id.
+                IdentityManager.getDefaultIdentityManager().getUserID(new IdentityHandler() {
+                    @Override
+                    public void onIdentityId(String identityId) {
+                        Log.d("YourMainActivity", "Identity ID = " + identityId);
+
+                        // Use IdentityManager#getCachedUserID to
+                        //  fetch the locally cached identity id.
+                        final String cachedIdentityId =
+                                IdentityManager.getDefaultIdentityManager().getCachedUserID();
+                    }
+
+                    @Override
+                    public void handleError(Exception exception) {
+                        Log.d("YourActivity", "Error in retrieving the identity" + exception);
+                    }
+                });
+            }
+        }).execute();
+        uploadWithTransferUtility();
+
+
+
+
+
+        // Removed to test video uploading
+        /*
         // Get ticket ID
         receiveTicketID();
 
@@ -60,8 +111,81 @@ public class AddTicketActivity extends AppCompatActivity{
 
         // Initialize views
         initViews();
+        */
 
     }
+
+
+
+    private void uploadWithTransferUtility() {
+
+        String OLD_PATHNAME = "/path/to/file/localFile.txt";
+        String FILENAME = "GKU000097.mp4";
+        String PATHNAME = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera/" + FILENAME;
+
+        TransferUtility transferUtility =
+                TransferUtility.builder()
+                        .context(getApplicationContext())
+                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .build();
+
+        TransferObserver uploadObserver =
+                transferUtility.upload(
+                        "addrequest-deployments-mobilehub-1269242402",
+                        FILENAME,
+                        new File(PATHNAME));
+
+        // Attach a listener to the observer to get state update and progress notifications
+        uploadObserver.setTransferListener(new TransferListener() {
+
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (TransferState.COMPLETED == state) {
+                    // Handle a completed upload.
+                    Log.d("YourActivity", "complete");
+
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+                int percentDone = (int)percentDonef;
+
+                Log.d("YourActivity", "ID:" + id + " bytesCurrent: " + bytesCurrent
+                        + " bytesTotal: " + bytesTotal + " " + percentDone + "%");
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                // Handle errors
+                Log.d("YourActivity", "error:  " + ex);
+            }
+
+        });
+
+        // If you prefer to poll for the data, instead of attaching a
+        // listener, check for the state and progress in the observer.
+        if (TransferState.COMPLETED == uploadObserver.getState()) {
+            // Handle a completed upload.
+        }
+
+        Log.d("YourActivity", "Bytes Transferrred: " + uploadObserver.getBytesTransferred());
+        Log.d("YourActivity", "Bytes Total: " + uploadObserver.getBytesTotal());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
@@ -96,14 +220,18 @@ public class AddTicketActivity extends AppCompatActivity{
     }
 
 
+    //Removed for aws testing
     /**
      * Save the instance ticket ID in case of screen rotation or app exit
      */
+    /*
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mTicketId = viewModel.getTicketID();
     }
+    */
+
 
 
     /**

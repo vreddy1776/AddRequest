d0c18bb3922453f9ba55b914bdebab135418eb5e
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-18 12:10:04 -0500

    MainActivityTest - Added Espresso Tests

---
+++ app/src/androidTest/java/com/example/android/addrequest/MainActivityTest/TicketClickTest.java
@@ -0,0 +1,70 @@
+package com.example.android.addrequest.MainActivityTest;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import com.example.android.addrequest.AddTicketActivity;
+import com.example.android.addrequest.MainActivity;
+import com.example.android.addrequest.R;
+
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
+
+
+@RunWith(AndroidJUnit4.class)
+public class TicketClickTest {
+
+    private static final String TICKET_ID_KEY = "TicketId";
+    private static final int DEFAULT_TICKET_ID = -1;
+    private static final String PACKAGE_NAME = InstrumentationRegistry.getTargetContext().getPackageName();
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void stubAllExternalIntents() {
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, new Intent()));
+    }
+
+    @Test
+    public void clickTicket() {
+
+        onView(withId(R.id.recyclerViewTickets)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+        //onData(anything()).inAdapterView(withId(R.id.recyclerViewTickets)).atPosition(1).perform(click());
+        // intended(Matcher<Intent> matcher) asserts the given matcher matches one and only one
+        // intent sent by the application.
+        intended(allOf(isInternal(),
+            toPackage(PACKAGE_NAME),
+            hasComponent(AddTicketActivity.class.getName())));
+    }
+
+}

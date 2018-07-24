bb3668e7793b7bca597b2cd40443e38aa456de4b
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-23 22:18:32 -0500

    Added query to personal ticket requests

--- app/src/main/java/com/example/android/addrequest/MVVM/Profile/ProfileViewModel.java
+++ app/src/main/java/com/example/android/addrequest/MVVM/Profile/ProfileViewModel.java
@@ -10,7 +10,14 @@ import com.example.android.addrequest.AWS.DynamoDB.DynamoDB;
 import com.example.android.addrequest.Database.AppDatabase;
 import com.example.android.addrequest.Database.AppExecuters;
 import com.example.android.addrequest.Database.TicketEntry;
+import com.example.android.addrequest.MVVM.AddTicket.FirebaseDbTicket;
 import com.example.android.addrequest.SharedPreferences.UserProfileSettings;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.List;
 
@@ -29,10 +36,22 @@ public class ProfileViewModel extends AndroidViewModel{
 
     private Application application;
 
+    private ChildEventListener mChildEventListener;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+    Query mQuery;
+
     public ProfileViewModel(Application application) {
         super(application);
 
         this.application = application;
+
+        String userId = UserProfileSettings.getUserID(application);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("Tickets");
+        mQuery = mMessagesDatabaseReference.orderByChild("userId").equalTo(userId);
 
         database = AppDatabase.getInstance(this.getApplication());
         Log.d(TAG, "Actively retrieving the ticket from the DataBase");
@@ -49,6 +68,7 @@ public class ProfileViewModel extends AndroidViewModel{
         syncVolley.select(application);
         */
 
+        //attachDatabaseReadListener();
 
 
 
@@ -90,8 +110,82 @@ public class ProfileViewModel extends AndroidViewModel{
 
     }
 
+
     public LiveData<List<TicketEntry>> getTickets() {
         return tickets;
     }
+
+
+    private void attachDatabaseReadListener() {
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                    FirebaseDbTicket firebaseDbTicket = dataSnapshot.getValue(FirebaseDbTicket.class);
+                    final TicketEntry ticket = new TicketEntry(
+                            firebaseDbTicket.getTicketId(),
+                            firebaseDbTicket.getTicketTitle(),
+                            firebaseDbTicket.getTicketDescription(),
+                            firebaseDbTicket.getTicketDate(),
+                            firebaseDbTicket.getTicketVideoId(),
+                            firebaseDbTicket.getUserId(),
+                            firebaseDbTicket.getUserName(),
+                            firebaseDbTicket.getUserPhotoUrl());
+
+                    AppExecuters.getInstance().diskIO().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            database.ticketDao().insertTicket(ticket);
+                        }
+
+                    });
+
+                }
+
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    FirebaseDbTicket firebaseDbTicket = dataSnapshot.getValue(FirebaseDbTicket.class);
+                    final TicketEntry ticket = new TicketEntry(
+                            firebaseDbTicket.getTicketId(),
+                            firebaseDbTicket.getTicketTitle(),
+                            firebaseDbTicket.getTicketDescription(),
+                            firebaseDbTicket.getTicketDate(),
+                            firebaseDbTicket.getTicketVideoId(),
+                            firebaseDbTicket.getUserId(),
+                            firebaseDbTicket.getUserName(),
+                            firebaseDbTicket.getUserPhotoUrl());
+
+                    AppExecuters.getInstance().diskIO().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            database.ticketDao().updateTicket(ticket);
+                        }
+
+                    });
+
+                }
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    FirebaseDbTicket firebaseDbTicket = dataSnapshot.getValue(FirebaseDbTicket.class);
+                    final int ticketId = firebaseDbTicket.getTicketId();
+
+                    AppExecuters.getInstance().diskIO().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            database.ticketDao().deleteTicketById(ticketId);
+                        }
+                    });
+
+                }
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+                public void onCancelled(DatabaseError databaseError) {}
+            };
+            mQuery.addChildEventListener(mChildEventListener);
+        }
+    }
+
+
+
 
 }

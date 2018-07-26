051a56d05f8e37d476f38d935b7f32b3929617dd
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-25 22:52:26 -0500

    Adding viewtypes after rebase attempt

--- app/src/main/java/com/example/android/addrequest/MVVM/AddTicket/AddTicketActivity.java
+++ app/src/main/java/com/example/android/addrequest/MVVM/AddTicket/AddTicketActivity.java
@@ -131,6 +131,8 @@ public class AddTicketActivity extends AppCompatActivity{
 
 
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ticket);

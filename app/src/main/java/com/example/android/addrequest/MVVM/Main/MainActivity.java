201be654fcf801a3628c41d7e9fc39c469aec056
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-26 13:19:44 -0500

    working on login

--- app/src/main/java/com/example/android/addrequest/MVVM/Main/MainActivity.java
+++ app/src/main/java/com/example/android/addrequest/MVVM/Main/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.android.addrequest.MVVM.Main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.android.addrequest.MVVM.Login.LoginActivity;
+import com.example.android.addrequest.MVVM.TicketList.TicketListActivity;
 import com.example.android.addrequest.R;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    // New commit after rebase to 1 parent 0676892 commit 9383e433ff031143acbd1a70e826df586cd52592 - "Updating Video Player"
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +26,50 @@ public class MainActivity extends AppCompatActivity {
         // Set Actionbar Title
         getSupportActionBar().setTitle(R.string.main_activity_name);
 
-        AuthUI.getInstance().signOut(this);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        currentUser = mFirebaseAuth.getCurrentUser();
+
+
+        //AuthUI.getInstance().signOut(this);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                if(currentUser == null){
+                    goToLogin();
+                } else {
+                    goToTicketList();
+                }
+            }
+        }, 5000);
+
+
 
     }
 
-    public void goToLogin(View view){
+    public void loginButtonClick(View view){
+        if(currentUser == null){
+            goToLogin();
+        } else {
+            goToTicketList();
+        }
+    }
+
+    private void goToLogin(){
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
+
+    private void goToTicketList(){
+        Intent intent = new Intent(MainActivity.this, TicketListActivity.class);
+        startActivity(intent);
+    }
+
+
+
 
 
 }

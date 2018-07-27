f05204bae56403353bf25200b9497d8d6a2f037b
Vijay Reddy <vijaymail1776@gmail.com>
2018-07-26 22:32:44 -0500

    Updated chat to include profile pic

--- app/src/main/java/com/example/android/addrequest/MVVM/Chat/MessageAdapter.java
+++ app/src/main/java/com/example/android/addrequest/MVVM/Chat/MessageAdapter.java
@@ -9,7 +9,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.android.addrequest.R;
+import com.example.android.addrequest.SharedPreferences.UserProfileSettings;
 
 import java.util.List;
 
@@ -27,6 +29,7 @@ public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
         ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
         TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
         TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+        ImageView profilePicView = (ImageView) convertView.findViewById(R.id.messageProfilePic) ;
 
         FriendlyMessage message = getItem(position);
 
@@ -43,6 +46,10 @@ public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
             messageTextView.setText(message.getText());
         }
         authorTextView.setText(message.getName());
+        Glide.with(getContext())
+                .load(UserProfileSettings.getUserPhotoURL(getContext()))
+                .apply(RequestOptions.circleCropTransform())
+                .into(profilePicView);
 
         return convertView;
     }

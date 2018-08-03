5fbe425d5b33efd291605fc890abc9ab25f80669
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-03 11:26:27 -0500

    Unit Tests - update

--- app/src/test/java/project/files/android/addrequest/IntegrationTests/TicketListActivityTest.java
+++ app/src/test/java/project/files/android/addrequest/IntegrationTests/TicketListActivityTest.java
@@ -1,6 +1,7 @@
 package project.files.android.addrequest.IntegrationTests;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -9,18 +10,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.fakes.RoboMenu;
 import org.robolectric.fakes.RoboMenuItem;
 
 import project.files.android.addrequest.BuildConfig;
+import project.files.android.addrequest.MVVM.AddTicket.AddTicketActivity;
 import project.files.android.addrequest.MVVM.TicketList.TicketListActivity;
 import project.files.android.addrequest.R;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.robolectric.Shadows.shadowOf;
 
 
@@ -29,8 +30,9 @@ import static org.robolectric.Shadows.shadowOf;
 @Config(constants = BuildConfig.class)
 public class TicketListActivityTest {
 
+
     @Test
-    public void menuTest() {
+    public void activityCreatedTest() {
 
         Activity activity = Robolectric.buildActivity(TicketListActivity.class).create().visible().get();
         assertNotNull(activity);
@@ -38,22 +40,22 @@ public class TicketListActivityTest {
         ImageView fab = activity.findViewById(R.id.fab);
         assertNotNull(fab);
 
-        //final Menu menu = shadowOf(activity).getOptionsMenu();
-        //assertNotNull(menu);
+        MenuItem menuItem = new RoboMenuItem(R.id.user_name_menu);
+        assertNotNull(menuItem);
 
-
-        //MenuItem menuItem = new RoboMenuItem(R.id.user_name_menu);
-        //assertNotNull(menuItem);
-
-        //CharSequence menuTitle = menuItem.getTitle();
-        //assertNotNull(menuTitle);
-
-
-        //assertThat(menuItem.getTitle()).isEqualTo("Sign Out");
-
-        //final Menu menu = shadowOf(activity).getOptionsMenu();
-        //assertThat(menu.findItem(R.id.sign_out_menu).getTitle()).isEqualTo("First menu item");
     }
+
+
+    @Test
+    public void fabClickTest() {
+        Activity activity = Robolectric.setupActivity(TicketListActivity.class);
+        activity.findViewById(R.id.fab).performClick();
+
+        Intent expectedIntent = new Intent(activity, AddTicketActivity.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+    }
+
 
 }
 

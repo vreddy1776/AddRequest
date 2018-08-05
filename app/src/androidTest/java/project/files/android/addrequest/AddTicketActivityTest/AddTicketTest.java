95768f1ac2afa34f581c95c8b741f178dfb4f3e6
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-05 12:35:36 -0500

    UI Tests - AddTicketActivity - Add Ticket

--- app/src/androidTest/java/project/files/android/addrequest/AddTicketActivityTest/AddTicketTest.java
+++ app/src/androidTest/java/project/files/android/addrequest/AddTicketActivityTest/AddTicketTest.java
@@ -1,5 +1,12 @@
 package project.files.android.addrequest.AddTicketActivityTest;
 
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -7,17 +14,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.view.View;
-
-import project.files.android.addrequest.MVVM.AddTicket.AddTicketActivity;
 import project.files.android.addrequest.MVVM.TicketList.TicketListActivity;
 import project.files.android.addrequest.R;
+import project.files.android.addrequest.Utils.StringGenerator;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -34,25 +33,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-/**
- * Tests for the notes screen, the main screen which contains a grid of all notes.
- */
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class AddTicketTest {
 
-    /**
-     * A custom {@link Matcher} which matches an item in a {@link RecyclerView} by its text.
-     *
-     * <p>
-     * View constraints:
-     * <ul>
-     * <li>View must be a child of a {@link RecyclerView}
-     * <ul>
-     *
-     * @param itemText the text to match
-     * @return Matcher that matches text in the given view
-     */
+
     private Matcher<View> withItemText(final String itemText) {
         checkArgument(!TextUtils.isEmpty(itemText), "itemText cannot be null or empty");
         return new TypeSafeMatcher<View>() {
@@ -70,18 +56,6 @@ public class AddTicketTest {
         };
     }
 
-    /**
-     * {@link ActivityTestRule} is a JUnit {@link Rule @Rule} to launch your activity under test.
-     *
-     * <p>
-     * Rules are interceptors which are executed for each test method and are important building
-     * blocks of Junit tests.
-     */
-    /*
-    @Rule
-    public ActivityTestRule<AddTicketActivity> mNotesActivityTestRule =
-            new ActivityTestRule<>(AddTicketActivity.class);
-            */
 
     @Rule
     public IntentsTestRule<TicketListActivity> mActivityRule = new IntentsTestRule<>(
@@ -89,44 +63,35 @@ public class AddTicketTest {
 
 
     @Test
-    public void clickAddNoteButton_opensAddNoteUi() throws Exception {
-        // Click on the add note button
-        onView(withId(R.id.fab)).perform(click());
+    public void clickFab_opensAddTicketUi() throws Exception {
 
-        // Check if the add note screen is displayed
+        onView(withId(R.id.fab)).perform(click());
         onView(withId(R.id.editTextTicketTitle)).check(matches(isDisplayed()));
     }
 
 
     @Test
-    public void addNoteToNotesList() throws Exception {
-        String newNoteTitle = "Espresso";
-        String newNoteDescription = "bab";
+    public void addTicketToTicketList() throws Exception {
+        String newNoteTitle =
+                StringGenerator.randomString(3) +
+                "-Title-" +
+                StringGenerator.randomString(3);
+        String newNoteDescription =
+                StringGenerator.randomString(5) +
+                "-Description-" +
+                StringGenerator.randomString(5);
 
-        // Click on the add note button
         onView(withId(R.id.fab)).perform(click());
 
-        // Add note title and description
-        // Type new note title
         onView(withId(R.id.editTextTicketTitle)).perform(typeText(newNoteTitle), closeSoftKeyboard());
         onView(withId(R.id.editTextTicketDescription)).perform(typeText(newNoteDescription),
-                closeSoftKeyboard()); // Type new note description and close the keyboard
+                closeSoftKeyboard());
 
-        // Save the note
         onView(withId(R.id.saveButton)).perform(click());
-
-
-
-        // Scroll notes list to added note, by finding its description
-
-
         onView(withId(R.id.recyclerViewTickets)).perform(
                 scrollTo(hasDescendant(withText(newNoteDescription))));
 
-
-        // Verify note is displayed on screen
         onView(withItemText(newNoteDescription)).check(matches(isDisplayed()));
-
 
     }
 
---
+++ app/src/main/java/project/files/android/addrequest/Utils/StringGenerator.java
@@ -0,0 +1,19 @@
+package project.files.android.addrequest.Utils;
+
+import java.util.Random;
+
+public class StringGenerator {
+
+    public static String randomString(int length) {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(length);
+        char tempChar;
+        for (int i = 0; i < length; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+}

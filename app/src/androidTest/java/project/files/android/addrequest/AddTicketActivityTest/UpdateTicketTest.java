ea450a625056fb1406903d5a040610ff03d545fb
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-05 13:16:05 -0500

    UI Tests - AddTicketActivity - Add Ticket

--- app/src/androidTest/java/project/files/android/addrequest/AddTicketActivityTest/UpdateTicketTest.java
+++ app/src/androidTest/java/project/files/android/addrequest/AddTicketActivityTest/UpdateTicketTest.java
@@ -1,96 +1,115 @@
 package project.files.android.addrequest.AddTicketActivityTest;
 
-import android.content.Intent;
-import android.database.Cursor;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
 
-import org.junit.Before;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import project.files.android.addrequest.Database.AppDatabase;
-import project.files.android.addrequest.MVVM.AddTicket.AddTicketActivity;
-import project.files.android.addrequest.MVVM.AddTicket.AddTicketViewModel;
 import project.files.android.addrequest.MVVM.TicketList.TicketListActivity;
 import project.files.android.addrequest.R;
-import project.files.android.addrequest.Utils.GlobalConstants;
+import project.files.android.addrequest.Utils.StringGenerator;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollTo;
+import static android.support.test.espresso.core.internal.deps.guava.base.Preconditions.checkArgument;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.assertEquals;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class UpdateTicketTest {
 
-    private static final String STRING_TO_BE_TYPED = "Light won't turn on!";
-    private AddTicketActivity mAddTicketActivity;
-    private TicketListActivity mTicketListActivity;
 
-    @Rule
-    public ActivityTestRule mTicketListActivityRule = new ActivityTestRule<>(
-            TicketListActivity.class,
-            true,
-            false);
+    private Matcher<View> withItemText(final String itemText) {
+        checkArgument(!TextUtils.isEmpty(itemText), "itemText cannot be null or empty");
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View item) {
+                return allOf(
+                        isDescendantOfA(isAssignableFrom(RecyclerView.class)),
+                        withText(itemText)).matches(item);
+            }
 
-    @Rule
-    public ActivityTestRule mAddTicketActivityRule = new ActivityTestRule<>(
-            AddTicketActivity.class,
-            true,
-            false);
-
-    @Before
-    public void intentToActivity() {
-
-        Intent intent = new Intent();
-        mTicketListActivityRule.launchActivity(intent);
-
-        mTicketListActivity = (TicketListActivity) mTicketListActivityRule.getActivity();
-
-        //mTicketListActivity.onItemClickListener();
-
-        /*
-        Intent intent = new Intent();
-        intent.putExtra(GlobalConstants.TICKET_TYPE_KEY, GlobalConstants.ADD_TICKET_TYPE);
-        mAddTicketActivityRule.launchActivity(intent);
-
-        mAddTicketActivity = (AddTicketActivity) mAddTicketActivityRule.getActivity();
-        */
-
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is isDescendantOfA RV with text " + itemText);
+            }
+        };
     }
 
+
+    @Rule
+    public IntentsTestRule<TicketListActivity> mActivityRule = new IntentsTestRule<>(
+            TicketListActivity.class);
+
+
     @Test
-    public void updateTicket() {
+    public void clickItem_opensAddTicketUi() throws Exception {
 
-        onView(withId(R.id.recyclerViewTickets)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-
-
-        /*
-        onView(withId(R.id.editTextTicketTitle))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
-
-        onView(withId(R.id.saveButton))
-                .perform(click());
-
-        AddTicketViewModel addTicketViewModel = mAddTicketActivity.getViewModel();
-        int ticketId = mAddTicketActivity.getTicketId();
-        AppDatabase actualAppDatabase = addTicketViewModel.getAppDatabase();
-
-        Cursor cursor = actualAppDatabase.query("SELECT * FROM ticket WHERE ticketId = " + ticketId,null);
-        String actualTicketTitle = "fail";
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            actualTicketTitle = cursor.getString(cursor.getColumnIndex("ticketTitle"));
+        try {
+            onView(withId(R.id.user_name_menu)).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+            onView(withText(R.string.my_requests)).perform(click());
         }
 
-        String expectedTicketTitle = STRING_TO_BE_TYPED;
-        assertEquals(expectedTicketTitle,actualTicketTitle);
-        */
+        onView(withId(R.id.recyclerViewTickets)).perform
+                (RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+        onView(withId(R.id.editTextTicketTitle)).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void updateTicketOnTicketList() throws Exception {
+
+        String newTitle = StringGenerator.randomTitle();
+        String newDescription = StringGenerator.randomDescription();
+
+        try {
+            onView(withId(R.id.user_name_menu)).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+            onView(withText(R.string.my_requests)).perform(click());
+        }
+
+        onView(withId(R.id.recyclerViewTickets)).perform
+                (RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+        onView(withId(R.id.editTextTicketTitle)).perform(clearText(), typeText(newTitle), closeSoftKeyboard());
+        onView(withId(R.id.editTextTicketDescription)).perform(clearText(), typeText(newDescription),
+                closeSoftKeyboard());
+
+        onView(withId(R.id.saveButton)).perform(click());
+        onView(withId(R.id.recyclerViewTickets)).perform(
+                scrollTo(hasDescendant(withText(newDescription))));
+
+        onView(withItemText(newDescription)).check(matches(isDisplayed()));
 
     }
 

a1594b932646cd293a7009e3a85ab8c40e466717
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-06 08:00:32 -0500

    1st commit after rebase to origin/master

--- app/src/main/java/project/files/android/addrequest/MVVM/Main/MainActivity.java
+++ app/src/main/java/project/files/android/addrequest/MVVM/Main/MainActivity.java
@@ -28,6 +28,8 @@ import project.files.android.addrequest.Settings.UserProfileSettings;
 import static com.firebase.ui.auth.ui.AcquireEmailHelper.RC_SIGN_IN;
 
 
+//  1st commit after rebase to master
+
 /**
  * Main Activity
  *

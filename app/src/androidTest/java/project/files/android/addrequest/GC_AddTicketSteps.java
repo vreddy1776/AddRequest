d2e35d3afcdd341f66526e15df23d2d629ed46a0
Vijay Reddy <vijaymail1776@gmail.com>
2018-09-02 18:56:07 -0500

    AddTicket BDD Test

--- app/src/androidTest/java/project/files/android/addrequest/GC_AddTicketSteps.java
+++ app/src/androidTest/java/project/files/android/addrequest/GC_AddTicketSteps.java
@@ -5,17 +5,25 @@ import com.mauriciotogneri.greencoffee.annotations.Given;
 import com.mauriciotogneri.greencoffee.annotations.Then;
 import com.mauriciotogneri.greencoffee.annotations.When;
 
+import project.files.android.addrequest.Utils.StringUtils;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 public class GC_AddTicketSteps extends GreenCoffeeSteps
 {
+
+    String newTitle = StringUtils.randomTitle();
+    String newDescription = StringUtils.randomDescription();
+
     @Given("^I see an empty login form$")
     public void iSeeAnEmptyLoginForm()
     {
-        onView(withId(R.id.fab)).perform(click());
-
+        onView(withText(newDescription)).check(doesNotExist());
     }
 
     @When("^I introduce an invalid username$")
@@ -27,18 +35,18 @@ public class GC_AddTicketSteps extends GreenCoffeeSteps
     @When("^I introduce an invalid password$")
     public void iIntroduceAnInvalidPassword()
     {
-        onView(withId(R.id.fab)).perform(click());
+        //onView(withId(R.id.fab)).perform(click());
     }
 
     @When("^I press the login button$")
     public void iPressTheLoginButton()
     {
-        onView(withId(R.id.fab)).perform(click());
+        //onView(withId(R.id.fab)).perform(click());
     }
 
     @Then("^I see an error message saying 'Invalid credentials'$")
     public void iSeeAnErrorMessageSayingInvalidCredentials()
     {
-        onView(withId(R.id.fab)).perform(click());
+        //onView(withId(R.id.fab)).perform(click());
     }
 }
